3d4f268f2821cfba5a330afa14f9ecaa59951df2
EchoAgent <91441032+Liamversionone@users.noreply.github.com>
2022-06-03 07:13:12 -0400

    Update Sizer.java

--- Sizer.java
+++ Sizer.java
@@ -8,10 +8,13 @@ public class Sizer {
 	boolean complete;
 	boolean[][] lights;
 	
+	//Getters
 	public int getRow() {return r;}
 	public int getCol() {return c;}
 	public boolean[][] getArray() {return lights;}
 	
+	//This constructor is used to create a 2D boolean array that holds the values that are used to determine if a cell is alive or dead. It asks the user what
+	//dimensions they would like to use.
 	public Sizer() {
 		complete = false;
 		while(!complete) {
